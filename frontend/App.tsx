154f126a6267b7683df538cc12c28581bb6c10d1
Nikita Butnar <nick.butnar@jetrockets.com>
2025-02-23 04:58:20 +0400

    cleniest way of auth auth flow

---
+++ frontend/App.tsx
@@ -0,0 +1,40 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { AuthProvider, useAuth } from "./app/context/AuthContext";
+import { Button } from "react-native";
+import Home from './app/screens/Home';
+import Login from './app/screens/Login';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <AuthProvider>
+      <Layout />
+    </AuthProvider>
+  );
+}
+
+export const Layout = () => {
+  const { authState, onLogout } = useAuth();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        {authState?.authenticated ? (
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{
+              headerRight: () => (
+                <Button title="Sign out" onPress={onLogout} />
+              ),
+            }}
+          />
+        ) : (
+          <Stack.Screen name="Login" component={Login} />
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
