e2107b6dcf84235e621b40c1dfa1b992cf37c889
Nikita Butnar <nick.butnar@jetrockets.com>
2025-03-04 14:38:02 +0400

    header component for home screen

--- frontend/app/navigation/AppTabs.tsx
+++ frontend/app/navigation/AppTabs.tsx
@@ -2,6 +2,7 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { FontAwesome5 } from "@expo/vector-icons";
 import Home from "../screens/Home";
 import Profile from "../screens/Profile";
+import HomeHeader from "../../components/HomeHeader";
 
 const Tab = createBottomTabNavigator();
 
@@ -17,11 +18,35 @@ export default function AppTabs() {
         name="Home"
         component={Home}
         options={{
+          header: () => <HomeHeader />,
           tabBarIcon: ({ color, size }) => (
             <FontAwesome5 name="home" size={size} color={color} />
           ),
         }}
       />
+
+      <Tab.Screen
+        name="Create post"
+        component={Home}
+        options={{
+          header: () => <HomeHeader />,
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome5 name="plus" size={size} color={color} />
+          ),
+        }}
+      />
+
+      <Tab.Screen
+        name="Favorites"
+        component={Home}
+        options={{
+          header: () => <HomeHeader />,
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome5 name="heart" size={size} color={color} />
+          ),
+        }}
+      />
+
       <Tab.Screen
         name="Profile"
         component={Profile}
